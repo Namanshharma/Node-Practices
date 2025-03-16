941df1da649f7ca2f4462aa5b2a1b4a0c2b56213
Naman Sharma <namansharma812@gmail.com>
2025-03-16 13:41:11 +0530

    Node initilization

---
+++ Initial Practices/server.js
@@ -0,0 +1,7 @@
+// ==> Initialize the NPM by using npm init -y ( will automatically configure ) <<----- through this we get the Package.json file 
+// ==> Install express by using npm i Express
+// ==> What is Transitive dependencies  <<----- Dependency dependent on some other dependency
+// ==> package.json is used to manage the updates of package
+
+// Problem with Node due to which we move to Express ( framework of Node ) :- Very complex to initialzie
+// :- Routing and middleware makes easy
