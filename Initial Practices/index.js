a9c9025ba86f95a8106cfee72d01742768237100
Naman Sharma <namansharma812@gmail.com>
2025-03-20 01:39:11 +0530

    versioning

--- Initial Practices/index.js
+++ Initial Practices/index.js
@@ -1,12 +1,18 @@
-const http = require('http');
-const fs = require('fs');
-const url = require('url'); // this package is used to handle the Request URL completely with the route parameter, query string parameter.
+const express = require('express');
 
-const app = http.createServer((req, res) => {
-    const myUrl = url.parse(req.url, true, true);
-    console.log(myUrl);
-    // fs.appendFile('./LogFile.txt', `${Date.now()} : New Request has received at : ${req.url}\n`, () => { })
-    res.end("Hello from Server");
-});
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.send("Hey from Home page");
+})
+
+
+
+// const app = http.createServer((req, res) => {    // traditional way to build server in Node by using Http module
+//     const myUrl = url.parse(req.url, true, true);
+//     console.log(myUrl);
+//     // fs.appendFile('./LogFile.txt', `${Date.now()} : New Request has received at : ${req.url}\n`, () => { })
+//     res.end("Hello from Server");
+// });
 
 app.listen(4000, () => console.log("Server is running on Port : 4000"))
