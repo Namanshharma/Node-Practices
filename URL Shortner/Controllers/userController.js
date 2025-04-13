1b203d7145a742cc6df1d60fae517d9a1efd7a39
Naman Sharma <namansharma812@gmail.com>
2025-04-14 03:49:57 +0530

    Create Hashmap

--- URL Shortner/Controllers/userController.js
+++ URL Shortner/Controllers/userController.js
@@ -1,4 +1,6 @@
 const User = require('../Models/usersModel');
+const { v4: uuidv4 } = require('uuid');
+const { getUser, setUser } = require('../Service/auth')
 
 const userSignUpHandler = async (req, res) => {
     const { name, email, password } = req.body;
@@ -10,6 +12,9 @@ const userLoginHandler = async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email, password });
     if (!user) res.render('login', { error: "Invalid Email or Password" });
+    const sessionId = uuidv4();
+    setUser(sessionId, user);
+    res.cookie('uid', sessionId);
     return res.render("home");
 };
 
